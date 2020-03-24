0502a34df5a12ca4bf31a4bd7f307396d55531f7
Igor <26maius@gmail.com>
2020-03-24 21:49:07 +0400

    added comment to models.py

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+# Created Model Post
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
