82dc2bdb8b3db3f54092bc63f2a93aa84a6196ff
Igor <26maius@gmail.com>
2020-04-16 19:30:52 +0400

    changed settings and Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn appdjangovenv.wsgi --log-file -
+web: gunicorn mysite.wsgi --log-file -
--- mysite/settings.py
+++ mysite/settings.py
@@ -90,8 +90,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#DATABASES['default'].update(dj_database_url.config(conn_max_age=500, require_ssl=True))
 
 
 # Password validation
@@ -130,13 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/app/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-MEDIA_URL = '/app/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
