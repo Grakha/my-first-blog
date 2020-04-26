56bc0c568babee42f65a87445f6d96e08f0203d7
Igor <26maius@gmail.com>
2020-04-26 21:11:38 +0400

    local settings added import

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,15 +1,11 @@
 import os
-
+from .settings import *
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': 'giobee',
-        'PASSWORD': 'Adminceka26$',
-        'HOST': '127.0.0.1',
-        'PORT': '',
     }
 }
 
